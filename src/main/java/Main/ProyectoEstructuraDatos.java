f9cd75b3542c3ca1bb1979b1219fc546b3b1d49b
UGALDEMMJ <151679842+UGALDEMMJ@users.noreply.github.com>
2024-11-03 16:26:49 -0600

    Creacion del Repositorio

---
+++ src/main/java/Main/ProyectoEstructuraDatos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Main;
+
+/**
+ *
+ * @author Marcos
+ */
+public class ProyectoEstructuraDatos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
