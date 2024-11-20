378ea1f9122b24382ef56ef3359f3ac6c602135f
Marc0sUM <marcosugaldemo@gmail.com>
2024-11-19 18:15:22 -0600

    Se agregan clases y funcionalidades del modulo 2 Con mi cuenta de U

---
+++ src/main/java/Main/colaPersonas.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+
+/**
+ *
+ * @author Marcos
+ */
+public class colaPersonas {
+    private NodoC primero;
+    private NodoC ultimo;
+    
+    public boolean isEmpty(){
+        return primero == null;
+    }
+    
+    
+    
+}
